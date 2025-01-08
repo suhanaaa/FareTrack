bb91f664c53a315a10a01a8d31ee61a055234076
Your Name <you@example.com>
2025-01-08 20:35:20 +0530

    randomm changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,32 +5,15 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
   async rewrites() {
     return [
       {
-        source: "/api/flight-search",
+        source: "/api/flights/:path*",
         destination: "https://travel.paytm.com/api/a/flights/v1/:path*",
+        basePath: false,
       },
     ];
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
